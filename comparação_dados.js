665819074f2c731a4b72566150bfbb02f9d1bbca
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-04 19:01:15 -0300

    initial commit

---
+++ comparação_dados.js
@@ -0,0 +1,21 @@
+const GE = {
+	A: '+',
+	B: '-',
+	C: '-'
+}
+const 1 = {
+	A: '-',
+	B: '+',
+	C: '-'	
+}
+
+
+
+function compare (anc, obj1, obj2, c1, c2){
+	if (anc.c1 == obj1.c1){
+		return 'sin'}
+	if (obj1.c1 == ob2.c1){
+		return 'ples'}
+	if (!anc.c1 == obj1.c1){
+		return 'apo'}
+}
