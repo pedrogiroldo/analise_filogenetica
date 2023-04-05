c53721bd83f34ddfe9c463f2813dff56c3a186ed
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-04 21:30:50 -0300

    nada dms

--- comparação_dados.js
+++ comparação_dados.js
@@ -11,11 +11,13 @@ const one = {
 
 
 
-function compare (anc, obj1, obj2, c1, c2){
+function compare (anc, obj1, obj2,){
 	if (anc.c1 == obj1.c1){
-		return 'sin'}
-	if (obj1.c1 == ob2.c1){
-		return 'ples'}
+		return 'sin', console.log('sin')}
+	if (obj1.c1 == obj2.c1){
+		return 'ples', console.log('ples')}
 	if (!anc.c1 == obj1.c1){
-		return 'apo'}
+		return 'apo', console.log('apo')}
 }
+
+compare(GE, one)
