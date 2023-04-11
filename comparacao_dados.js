48d09db2c8ce0162e3919abe84944c34d6db4d3c
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-11 16:59:51 -0300

    initial commit

---
+++ comparacao_dados.js
@@ -0,0 +1,49 @@
+
+// constantes universais
+const cnv = document.getElementById('cladograma');
+const ctx = cnv.getContext('2d');
+
+const WIDTH = cnv.width/1.25
+const HEIGHT = cnv.height/1.25
+
+ctx.strokeStyle = 'black'
+ctx.strokeRect(10, 10, WIDTH, HEIGHT)
+
+//desenho testes
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(20, 20);
+ctx.lineTo(50, 50);
+ctx.stroke();
+
+
+
+
+
+
+const GE = {
+	A: '+',
+	B: '-',
+	C: '-'
+}
+const one = {
+	A: '-',
+	B: '+',
+	C: '-'	
+}
+
+
+
+function compare (anc, obj1, obj2, c){
+	if (anc[c] == obj1[c]){
+	return 'sin'}
+	if (obj1[c] == obj2[c]){
+		return 'ples'}
+	if (anc[c] == obj1[c]){
+		return 'apo'}
+}
+
+
+// testes
+console.log(WIDTH)
+console.log(HEIGHT)
